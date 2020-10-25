8b967cc4f56aae71091594b71092a90d44633157
Jorge <jorgesifontes.cl@gmail.com>
2020-10-24 21:46:59 -0300

    Show Winner msg

--- piedra_papel_tijera_avanzado.rb
+++ piedra_papel_tijera_avanzado.rb
@@ -1,87 +1,98 @@
-def clear_screen ()    #clear screen only in Visual Studio Code
+def print_menu ()
+    print '
+    __     __    ________    ___    __    __     __
+   |  \   /  |  |    ____|  |   \  |  |  |  |   |  |
+   |   \_/   |  |   |____   |    \_|  |  |  |   |  |
+   |         |  |    ____|  |   _     |  |  |   |  |
+   |  |\_/|  |  |   |____   |  | \    |  |  \___/  |
+   |__|   |__|  |________|  |__|  \___|   \_______/ '
+end
+
+def print_winner ()
+    print '
+    __     __    ________    ___    __    ___    __    ________    _______
+   |  | _ |  |  |__    __|  |   \  |  |  |   \  |  |  |    ____|  |   __  \
+   |  |/ \|  |     |  |     |    \_|  |  |    \_|  |  |   |____   |  |__|  |
+   |    _    |     |  |     |   _     |  |   _     |  |    ____|  |   _   /
+   |   / \   |   __|  |__   |  | \    |  |  | \    |  |   |____   |  | \  \
+   |__/   \__|  |________|  |__|  \___|  |__|  \___|  |________|  |__|  \__\ ' 
+end
+
+def print_player (player)
+
+    if (player == 1)
+        print '
+                             _______     
+                            |   __  \       111 
+                            |  |__|  |    11111    
+                            |   ____/       111 
+                            |  |            111 
+                            |__|            111'
+    else
+        print '
+                             _______     
+                            |   __  \      22222 
+                            |  |__|  |    22   22  
+                            |   ____/         22
+                            |  |            222
+                            |__|          2222222'
+    end
+    print "\n\n\n"
+end
+
+def clear_screen ()    #Limpia la pantalla solo en Visual Studio Code
     print "\e[2J\e[f"
 end
 
-def opc_invalid (opc)
+def opc_invalid (opc)  #Valida la opcion del menu
     !((opc >= 1) && (opc <= 4))
 end
 
-def show_menu (player)
+def show_menu (player, invalid_opc)       #Muestra menu
+    
+    print_menu()
 
-print '
-     __     __    ________    ___    __    __     __
-    |  \   /  |  |    ____|  |   \  |  |  |  |   |  |
-    |   \_/   |  |   |____   |    \_|  |  |  |   |  |
-    |         |  |    ____|  |   _     |  |  |   |  |
-    |  |\_/|  |  |   |____   |  | \    |  |  \___/  |
-    |__|   |__|  |________|  |__|  \___|   \_______/
-'
-
-print "\nTurno jugador "
+    print "\nTurno jugador "
     if player == 1
-        puts "uno \n"
+        print "uno"
     else
-        puts "dos \n"
+        print "dos"
     end
 
-puts "1. Piedra"
-puts "2. Papel"
-puts "3. Tijeras"
-puts "4. Salir"
+    if invalid_opc
+        puts "     Ingrese una Opcion Valida"
+    else
+        puts " "
+    end
+
+    puts "1. Piedra"
+    puts "2. Papel"
+    puts "3. Tijeras"
+    puts "4. Salir"
 
 end
 
 def winner (opc_p1,opc_p2)
 
-    if (opc_p1 == opc_p2)
+    if (opc_p1 == opc_p2)                  #Empate
         0
     else
-        piedra = (opc_p1 == 1 && opc_p2 == 3)
-        papel = (opc_p1 == 2 && opc_p2 == 1)
-        tijera = (opc_p1 == 3 && opc_p2 == 2)
+        piedra = ((opc_p1 == 1) && (opc_p2 == 3))
+        papel = ((opc_p1 == 2) && (opc_p2 == 1))
+        tijera = ((opc_p1 == 3) && (opc_p2 == 2))
 
-        if (piedra || papel || tijera)
+        if (piedra || papel || tijera)     #Winner player 1
             1
-        else
+        else                               #Winner player 2
             2
         end
     end
 end
 
-
-
-=begin
-
-puts "                                                                              ████          ████   "
-puts "                                                  ██▒▒                      ▓▓░░░░▓▓      ▓▓░░░░▓▓ "
-puts "                                            ██▓▓▓▓░░░░▓▓▓▓██                ██░░░░██      ██░░░░██ "
-puts "                                          ██░░░░██░░░░██░░░░██                ██░░░░██    ██░░░░██ "
-puts "                                      ▓▓▓▓██░░░░██░░░░██░░░░██                ██░░░░██  ░░░░░░░░██ "
-puts "                                    ▓▓░░░░██░░░░██░░░░██░░░░██                ██░░░░██  ██░░░░▓▓  "
-puts "          ▓▓▓▓  ▓▓▓▓  ██▒▒          ██░░░░██░░░░██░░░░██░░░░██              ▓▓██▓▓░░░░▓▓██░░░░██  "
-puts "    ██████░░░░██░░░░██░░░░██        ██░░░░██░░░░██░░░░██░░░░██        ██████░░░░██░░░░██░░░░██    "
-puts "  ▓▓░░░░██░░░░██░░░░██░░░░██        ██░░░░██░░░░██░░░░██░░░░██      ██░░░░██░░░░██░░░░██░░░░██    "
-puts "  ▓▓░░░░██░░░░██░░░░██░░░░██        ██░░░░██░░░░██░░░░██░░░░██      ██░░░░██░░░░██░░░░░░░░░░██    "
-puts "  ▓▓░░░░██░░░░██░░░░██░░░░██        ██░░░░░░░░░░░░░░░░░░░░░░██      ██░░░░██░░░░██░░░░░░░░░░██    "
-puts "  ██░░░░██░░░░██████████████        ▓▓░░░░░░░░░░██████████████      ██░░░░██░░░░████████████▓▓    "
-puts "  ▓▓░░░░██░░░░██░░░░░░░░░░░░██      ▓▓░░░░░░░░░░██░░░░░░░░░░░░██    ▓▓░░░░██░░░░██░░░░░░░░░░░░██  "
-puts "  ▓▓▓▓▓▓▒▒▓▓▓▓██░░░░░░░░░░░░██      ██░░░░░░░░░░██░░░░░░░░░░░░██    ██▓▓▓▓▒▒▓▓▓▓██░░░░░░░░░░░░██  "
-puts "  ▓▓░░░░░░░░░░░░██▒▒▓▓▓▓░░░░██      ██░░░░░░░░░░░░▓▓▒▒▓▓▓▓░░░░▓▓    ▓▓░░▒▒░░░░░░░░▓▓▓▓▒▒▒▒░░░░██  "
-puts "  ░░░░░░░░░░░░░░░░▒▒░░░░░░░░██      ██░░░░░░░░░░░░░░░░░░░░░░░░▓▓    ▓▓░░░░░░░░░░░░░░░░░░░░░░░░██  "
-puts "  ░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓      ██░░░░░░░░░░░░░░░░░░░░░░░░██    ██░░░░░░░░░░░░░░░░░░░░░░░░██  "
-puts "  ░░░░░░░░░░░░░░░░░░░░░░░░██        ██░░░░░░░░░░░░░░░░░░░░░░██      ██░░░░░░░░░░░░░░░░░░░░░░██    "
-puts "  ██░░░░░░░░░░░░░░░░░░░░██          ██░░░░░░░░░░░░░░░░░░░░██        ██░░░░░░░░░░░░░░░░░░░░██    "
-puts "  ██░░░░░░░░░░░░░░░░░░██            ██░░░░░░░░░░░░░░░░░░██          ██░░░░░░░░░░░░░░░░░░██      "
-puts "  ██░░░░░░░░░░░░░░██                ██░░░░░░░░░░░░░░██              ██░░░░░░░░░░░░░░██        "
-puts "  ██░░░░░░░░░░░░░░██                ██░░░░░░░░░░░░░░██              ██░░░░░░░░░░░░░░██        "
-puts "  ██░░░░░░░░░░░░░░██                ██░░░░░░░░░░░░░░██              ██░░░░░░░░░░░░░░██        "
-puts "  ██░░░░░░░░░░░░░░██                ██░░░░░░░░░░░░░░██              ██░░░░░░░░░░░░░░██        "
-puts "  ██░░░░░░░░░░░░░░██                ██░░░░░░░░░░░░░░██              ██░░░░░░░░░░░░░░██        "
-puts "  ██░░░░░░░░░░░░░░██                ██░░░░░░░░░░░░░░██              ██░░░░░░░░░░░░░░██        "
-puts "  ██░░░░░░░░░░░░░░██                ██░░░░░░░░░░░░░░██              ██░░░░░░░░░░░░░░██        "
-puts "  ██░░░░░░░░░░░░░░██                ██░░░░░░░░░░░░░░██              ██░░░░░░░░░░░░░░██        "
-
-=end
+def show_winner(win_player)
+    print_winner()
+    print_player(win_player)    
+end
 
 =begin "
                                                                              ████          ████  
@@ -173,36 +184,50 @@ _,aad8P""''       ,d8888b   )   (  (   (    (  ,d8888P"'    __,,aadd8PP8
 
 =end
 
-
 current_opc = 0
 opc_player_1 = 0
 opc_player_2 = 0
 current_player = 1
 end_game = false
 close_menu = false
+show_invalid = false
 
 #Menu & Validation 
 
 while ((opc_invalid(opc_player_1) || opc_invalid(opc_player_2)) && (!close_menu) && (!end_game))
+    
     clear_screen()
 
-    show_menu (current_player)
+    show_menu(current_player,show_invalid)       #Muestra Menu
     current_opc = gets.to_i
 
-    if (!opc_invalid(current_opc))
-        if current_opc == 4
+    if (!opc_invalid(current_opc))               #Opcion es Valida
+        if current_opc == 4                      #Caso Salir
             end_game = true
-        elsif current_player == 1
+        elsif current_player == 1                #Guardar Opcion Player 1
             opc_player_1 = current_opc
             current_player = 2
-        elsif current_player == 2
+        elsif current_player == 2                #Guardar Opcion Player 2
             opc_player_2 = current_opc
             close_menu  = true
-        else                               #Esto nunca deberia pasar
+        else                                     #Esto nunca deberia pasar
             clear_screen()
             current_player = 1
         end
+        show_invalid = false
+    else                                         #Proximo refresh mostrara Opcion Invalida
+        show_invalid = true
     end
 end
 
-puts winner(opc_player_1,opc_player_2)
+if !end_game
+    result = winner(opc_player_1,opc_player_2)
+
+    clear_screen()
+
+    if result == 0
+        
+    else
+        show_winner(result)
+    end
+end
